5ef011a599ef0484aa94cd5fbd6002934c795d48
nicorus-79 <sadayasu.nagashima@icloud.com>
2022-03-31 20:22:02 +0900

    microCMSからコンテンツを取得するデモの追加

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+      <h1>写真一覧</h1>
+      <el-table
+        :data="items">
+        <el-table-column>
+          <template slot-scope="scope">
+           <nuxt-link :to="'/items/'+scope.row.id"><img :src="scope.row.image.url" /></nuxt-link>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+<script>
+ export default {
+  data () {
+    return {
+      items: []
+    }
+  },
+  async asyncData ({ $axios, params }) {
+    const { data } = await $axios.get('https://jamstack--demo.microcms.io/api/v1/photo', {
+      headers: { 'X-MICROCMS-API-KEY': 'd5a89b117ef64b58bc511f4089e8b0c84aca' }
+    })
+    return {
+      items: data.contents
+    }
+  },
+ }
+ </script>
---
+++ pages/items/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <el-row type="flex" :data="item">
+      <el-col :span="3">名称</el-col>
+      <el-col :span="10">説明</el-col>
+      <el-col :span="5">画像</el-col>
+    </el-row>
+    <hr />
+    <el-row type="flex" :data="item">
+      <el-col :span="3">{{item.name}}</el-col>
+      <el-col :span="10">{{item.explain}}</el-col>
+      <el-col :span="5"><img :src="item.image.url" /></el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+ export default {
+  async asyncData ({ $axios, params }) {
+    const { data } = await $axios.get('https://jamstack--demo.microcms.io/api/v1/photo/'+params.id, {
+      headers: { 'X-MICROCMS-API-KEY': 'd5a89b117ef64b58bc511f4089e8b0c84aca' }
+    })
+    return {
+      item: data
+    }
+  },
+ }
+ </script>
