31bf647a6f42f0e9f9fb0af3580c5f34fbfb15aa
nicorus-79 <sadayasu.nagashima@icloud.com>
2022-04-01 17:11:41 +0900

    SPAの設定に変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,7 @@ const { MICROCMS_API_KEY } = process.env
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
